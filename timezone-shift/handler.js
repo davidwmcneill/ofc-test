34d67c6271464de7589defb3c09d09f5dc846907
dmcneill <dmcneill@smartfocus.com>
2020-01-20 11:58:34 +0000

    test secret seal

--- timezone-shift/handler.js
+++ timezone-shift/handler.js
@@ -1,9 +1,19 @@
 "use strict"
 const moment = require('moment');
+const readline = require('readline');
+const fs = require('fs');
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('/var/openfaas/secrets/api-key'),
+    output: process.stdout,
+    console: false
+});
+
 module.exports = (event, context) => {
+    let secrets = readInterface()
     let meeting = moment.utc(event.body.meeting)
     let adjusted = meeting.clone().utc().add(-8, 'hours');
     context
         .status(200)
-        .succeed({ meeting: meeting.format(), adjusted: adjusted.format() });
+        .succeed({ meeting: meeting.format(), adjusted: adjusted.format(), secret: secrets.format() });
 }
